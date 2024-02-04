287670d0753c13c0d575b6f861adbeaa2884e00b
BHARGAV345 <139255000+BHARGAV345@users.noreply.github.com>
2024-02-04 11:06:39 +0530

    Update AdminLoginServlet.java

--- AdminLoginServlet.java
+++ AdminLoginServlet.java
@@ -1,20 +1,16 @@
 package com.sunbase.customer.servlets;
 
 import java.io.IOException;
-import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import com.sunbase.customer.dao.AdminLoginDAO;
 import com.sunbase.customer.dao.CustomerDAO;
 import com.sunbase.customer.dao.impl.AdminLoginDAOImpl;
 import com.sunbase.customer.dao.impl.CustomerDAOImpl;
-import com.sunbase.customer.model.Customers;
 
 @WebServlet("/AdminLoginServlet")
 public class AdminLoginServlet extends HttpServlet {
@@ -29,7 +25,8 @@ public class AdminLoginServlet extends HttpServlet {
         customerDAO = new CustomerDAOImpl();
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         // Get login details from the request
         String username = request.getParameter("username");
         String password = request.getParameter("password");
@@ -38,29 +35,16 @@ public class AdminLoginServlet extends HttpServlet {
         boolean isValidLogin = adminLoginDAO.validateAdminLogin(username, password);
 
         if (isValidLogin) {
-            // If login is successful, add admin to session and fetch the list of customers
+            // If login is successful, add admin to session
             HttpSession session = request.getSession();
             session.setAttribute("adminUsername", username);
 
-            // Fetch the list of customers
-//            List<Customers> customersList = customerDAO.getAllCustomers();
-//
-//            // Set the list of customers as an attribute in the session
-//            //session.setAttribute("customersList", customersList);
-//            request.setAttribute("searchResults", customersList);
-//            System.out.println(customersList);
-//            request.getRequestDispatcher("home.jsp").forward(request, response);
-            
             // Redirect to HomeServlet
             response.sendRedirect("HomeServlet");
-
-           // response.sendRedirect("home.jsp");
         } else {
             // If login fails, display error message on index.jsp
-            request.setAttribute("errorMessage", "Login failed. Please try again.");
+            request.setAttribute("errorMessage", "Invalid username or password. Please try again.");
             request.getRequestDispatcher("index.jsp").forward(request, response);
         }
     }
-    
-
 }
